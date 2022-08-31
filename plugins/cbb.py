834999ceb67de4962668c1e2cc0ecdda82112480
meyleens <112566708+meyleens@users.noreply.github.com>
2022-08-31 21:56:05 +0700

    Add files via upload

---
+++ plugins/cbb.py
@@ -0,0 +1,27 @@
+# (©)Codexbotz
+# Recode by @mrismanaziz
+# t.me/SharingUserbot & t.me/Lunatic0de
+
+from pyrogram.types import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup
+
+from bot import Bot
+from config import OWNER
+
+
+@Bot.on_callback_query()
+async def cb_handler(client: Bot, query: CallbackQuery):
+    data = query.data
+    if data == "about":
+        await query.message.edit_text(
+            text=f"<b>Join Group dan Channel Video Masa Kini:\n============\n1. @videomasakini\n2. @videomasakinigc\n\n",
+            disable_web_page_preview=True,
+            reply_markup=InlineKeyboardMarkup(
+                [[InlineKeyboardButton("Close", callback_data="close")]]
+            ),
+        )
+    elif data == "close":
+        await query.message.delete()
+        try:
+            await query.message.reply_to_message.delete()
+        except BaseException:
+            pass
